bda162a9f620fda3ff529227c9f97ea6c3c3d57d
lyoodong <dwryu3079@naver.com>
2025-07-25 15:52:24 +0900

    [add]Initial Commit

---
+++ app/EditingView.swift
@@ -0,0 +1,15 @@
+import SwiftUI
+
+struct EditingView: View {
+    var body: some View {
+        VStack {
+            Image(systemName: "globe")
+                .imageScale(.large)
+                .foregroundStyle(.tint)
+            Text("Hello, world!")
+        }
+        .padding()
+    }
+}
+
+
