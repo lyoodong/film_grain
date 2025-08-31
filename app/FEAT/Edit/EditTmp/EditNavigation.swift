86165327b62c7cfb2ae099e1f829da7edde82cbd
lyoodong <dwryu3079@naver.com>
2025-09-01 01:00:34 +0900

    [refactor] navigation

--- app/FEAT/Edit/EditTmp/EditNavigation.swift
+++ app/FEAT/Edit/EditTmp/EditNavigation.swift
@@ -1,44 +1,91 @@
 import SwiftUI
 
 struct EditNavigation: View {
-    let editVM: EditTmpViewModel
+    @ObservedObject var editVM: EditTmpViewModel
     @Environment(\.dismiss) var dismiss
-
+    
     private let diameter: CGFloat = 40
-
+    private let font = Poppin.semiBold.font(size: 20)
+    
     var body: some View {
         HStack(spacing: 12) {
-            Button {
-                dismiss()
-            } label: {
-                ZStack {
-                    Circle().fill(Color.mainGray)
-                    
-                    Image(systemName: "xmark")
-                        .font(Poppin.medium.font(size: 16))
-                        .foregroundColor(.white)
-                }
-                .frame(width: diameter, height: diameter)
-            }
-            .buttonStyle(.plain)
-            
+            xButton
             Spacer()
-
-            Button {
-                editVM.send(.saveButtonTapped)
-            } label: {
-                ZStack {
-                    Capsule()
-                        .fill(Color.mainGray)
-                    
-                    Image(systemName: "square.and.arrow.up")
-                        .font(Poppin.medium.font(size: 16))
-                        .foregroundColor(.white)
-                }
-                .frame(width: diameter, height: diameter)
-            }
-            .buttonStyle(.plain)
+            aiButton
+            saveButton
         }
         .padding(.horizontal, 16)
+        .padding(.bottom, 16)
+    }
+    
+    private var xButton: some View {
+        Button {
+            dismiss()
+        } label: {
+            Image(systemName: "xmark")
+                .font(font)
+                .foregroundColor(.white)
+                .frame(width: diameter, height: diameter)
+        }
+    }
+    
+    private var saveButton: some View {
+        Button {
+            editVM.send(.saveButtonTapped)
+        } label: {
+            Image(systemName: "square.and.arrow.up")
+                .font(font)
+                .foregroundColor(.white)
+                .frame(width: diameter, height: diameter)
+        }
+    }
+    
+    private var aiButton: some View {
+        Button {
+            editVM.send(.aiButtonTapped)
+        } label: {
+            Image(systemName: "apple.intelligence")
+                .font(font)
+                .foregroundColor(.pointRed)
+                .rotationEffect(.degrees(editVM.isAIAnalyzing ? 360.0 : 0.0))
+                .animation(aiAnimation, value: editVM.isAIAnalyzing)
+                .frame(width: diameter, height: diameter)
+        }
+    }
+    
+    private var aiAnimation: Animation {
+        editVM.isAIAnalyzing ? foreverAnimation : stopAnimation
+    }
+    
+    private var foreverAnimation: Animation {
+        .linear(duration: 1.0)
+        .repeatForever(autoreverses: false)
+    }
+    
+    private var stopAnimation: Animation {
+        .linear(duration: 0)
+    }
+}
+
+struct PressScaleButtonStyle: ButtonStyle {
+    var pressedScale: CGFloat = 0.96
+    var pressedBGColor: Color = .textGray
+    let diameter: CGFloat
+    
+    init(_ diameter: CGFloat) {
+        self.diameter = diameter
+    }
+
+    func makeBody(configuration: Configuration) -> some View {
+        configuration.label
+            .background(
+                Circle()
+                    .fill(pressedBGColor)
+                    .frame(width: diameter, height: diameter)
+                    .opacity(configuration.isPressed ? 1 : 0)
+            )
+            .contentShape(Circle())
+            .scaleEffect(configuration.isPressed ? pressedScale : 1.0)
+            .animation(.easeOut(duration: 0.12), value: configuration.isPressed)
     }
 }
