a09794c5c39d9774a3020c583b5747f35fe3ae1d
lyoodong <dwryu3079@naver.com>
2025-09-01 22:23:41 +0900

    [remove] navigation ai button

--- app/FEAT/Edit/EditNavigation.swift
+++ app/FEAT/Edit/EditNavigation.swift
@@ -12,7 +12,6 @@ struct EditNavigation: View {
             HStack(spacing: 12) {
                 xButton
                 Spacer()
-                aiButton
                 saveButton
             }
             
@@ -46,57 +45,8 @@ struct EditNavigation: View {
         }
     }
     
-    private var aiButton: some View {
-        Button {
-            editVM.send(.aiButtonTapped)
-        } label: {
-            Image(systemName: "apple.intelligence")
-                .font(font)
-                .foregroundColor(.pointRed)
-                .rotationEffect(.degrees(editVM.isAIAnalyzing ? 360.0 : 0.0))
-                .animation(aiAnimation, value: editVM.isAIAnalyzing)
-                .frame(width: diameter, height: diameter)
-        }
-    }
-    
-    private var aiAnimation: Animation {
-        editVM.isAIAnalyzing ? foreverAnimation : stopAnimation
-    }
-    
-    private var foreverAnimation: Animation {
-        .linear(duration: 1.0)
-        .repeatForever(autoreverses: false)
-    }
-    
-    private var stopAnimation: Animation {
-        .linear(duration: 0)
-    }
-    
     private var eidtToast: some View {
         Text(editVM.toast.text)
             .font(Poppin.medium.font(size: 12))
-    }
-}
-
-struct PressScaleButtonStyle: ButtonStyle {
-    var pressedScale: CGFloat = 0.96
-    var pressedBGColor: Color = .textGray
-    let diameter: CGFloat
-    
-    init(_ diameter: CGFloat) {
-        self.diameter = diameter
-    }
-    
-    func makeBody(configuration: Configuration) -> some View {
-        configuration.label
-            .background(
-                Circle()
-                    .fill(pressedBGColor)
-                    .frame(width: diameter, height: diameter)
-                    .opacity(configuration.isPressed ? 1 : 0)
-            )
-            .contentShape(Circle())
-            .scaleEffect(configuration.isPressed ? pressedScale : 1.0)
-            .animation(.easeOut(duration: 0.12), value: configuration.isPressed)
     }
 }
