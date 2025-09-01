492a6cfd934ada6d8fa63046f705f5b267616dd6
lyoodong <dwryu3079@naver.com>
2025-09-01 22:22:16 +0900

    [remove] origin edit codes

--- app/FEAT/Edit/EditOrigin/EditAiButton.swift
+++
@@ -1,9 +0,0 @@
-import SwiftUI
-
-struct EditAiButton: View {
-    @ObservedObject var editVM: EditViewModel
-
-    var body: some View {
-        Button("AI 추천") { editVM.send(.aiButtonTapped) }
-    }
-}
